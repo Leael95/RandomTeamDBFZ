90765668f0673194c984a1b73f10d16add286deb
Leael95 <leaxplosion@gmail.com>
2021-08-24 11:36:15 -0300

    Algoritmo funcionando

---
+++ Code/public/js/scripts.js
@@ -0,0 +1,22 @@
+let character1 = Math.floor(Math.random() * 43) + 1;
+let character2 = Math.floor(Math.random() * 43) + 1;
+let character3 = Math.floor(Math.random() * 43) + 1;
+
+let assist1 = Math.floor(Math.random() * 3) + 1;
+let assist2 = Math.floor(Math.random() * 3) + 1;
+let assist3 = Math.floor(Math.random() * 3) + 1;
+
+console.log(character1);
+console.log(character2);
+console.log(character3);
+
+console.log(assist1);
+console.log(assist2);
+console.log(assist3);
+
+document.write(character1);
+document.write(character2);
+document.write(character3);
+document.write(assist1);
+document.write(assist2);
+document.write(assist3);
